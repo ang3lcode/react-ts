157508315fcf67edee21e26cb9562890d915b831
angel melendez <angelmelen88@gmail.com>
2024-07-05 12:44:20 -0600

    tipos evntos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import type { MouseEventHandler } from "react";
 import { RandomFox } from "./components/RandomFox";
 
-const random = (): number => Math.floor(Math.random()*123)+1;
+const random = (): number => Math.floor(Math.random() * 123) + 1;
 const generateId = (): string => {
   return (
     Math.random().toString(36).substring(2, 15) +
@@ -9,23 +10,33 @@ const generateId = (): string => {
   );
 };
 
-type ImageItem = {id: string, url:string}
+type ImageItem = { id: string; url: string };
 
 function App(): JSX.Element {
-  const [images, setImages] = useState<Array<ImageItem>>([ // <string[]> or <Array<string>>
-   {id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
-
+  // <string[]> or <Array<string>>
+  const [images, setImages] = useState<Array<ImageItem>>([
+   
   ]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+
+    const newImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+    setImages([
+      ...images, newImageItem
+    ])
+  };
 
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-pink-600">Hello world!</h1>
-      
-      {images.map(({id, url}) => (
+      <button onClick={addNewFox}>Add new fox</button>
+      {images.map(({ id, url }) => (
         <div className="p-4" key={id}>
-          <RandomFox image={url}  />
+          <RandomFox image={url} /> 
         </div>
-
       ))}
     </>
   );
