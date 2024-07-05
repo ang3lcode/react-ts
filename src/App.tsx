f1013b30ce9761569907227aa162080739afe6d2
angel melendez <angelmelen88@gmail.com>
2024-07-05 11:44:21 -0600

    state con tipos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { RandomFox } from "./components/RandomFox";
 
 const random = (): number => Math.floor(Math.random()*123)+1
 
 function App(): JSX.Element {
+  const [images, setImages] = useState([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+  
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-pink-600">Hello world!</h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}  />
+      
+      {images.map((image, index) => (
+        <div className="p-4" key={index}>
+          <RandomFox image={image}  />
+        </div>
+
+      ))}
     </>
   );
 }
