9c7ca9c46e797dc667e132da1c92e1dd75616d1c
angel melendez <angelmelen88@gmail.com>
2024-07-05 18:37:58 -0600

    lazy loading con observadores

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,12 +1,32 @@
-import { useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
 
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-    const node = useRef<HTMLImageElement>(null)
+  const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+  useEffect(() => {
+    // nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        //onintersection -> consol
+        if (entry.isIntersecting) {
+          //   console.log("hey you");
+          setSrc(image);
+        }
+      });
+    });
+    // observador node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+    //desconectar
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
 
-    return (
-    <img ref={node} src={image} className="w-1/2  rounded-md" />
-  )
-}
+  return <img ref={node} src={src} className="w-1/2  rounded-md bg-gray-300" />;
+};
